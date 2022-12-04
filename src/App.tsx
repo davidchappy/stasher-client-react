5d924854fbde7319f713bc184ad847085c39f70a
David Chapman <davidchappy@gmail.com>
2022-12-03 21:58:59 -0600

    Logs for the win

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ type Goodie = {
 function App() {
   const { data, loading, error } = useQuery(GOODIES_QUERY)
 
+  console.log({ data, loading, error })
+
   if (error) return <p>Error : {error.message}</p>
 
   if (loading) return <p>Loading...</p>
