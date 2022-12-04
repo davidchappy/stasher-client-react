477d09b3ca0cb9f443ca08769541fb88549caeeb
David Chapman <davidchappy@gmail.com>
2022-12-03 21:54:10 -0600

    Try a fix for server.js

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-
-const express = require('express')
-const path = require("path");
+const express = require("express")
+const path = require("path")
 const app = express()
 
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
 // listed in the array.
 var options = {
-  dotfiles: 'ignore',
+  dotfiles: "ignore",
   etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
+  extensions: ["htm", "html", "css", "js", "ico", "jpg", "jpeg", "png", "svg"],
+  index: ["index.html"],
+  maxAge: "1m",
   redirect: false
 }
-app.use(express.static('build', options))
+
+console.log(path.join(__dirname, "build"))
+
+app.use(express.static(path.join(__dirname, "build"), options))
 
 const port = process.env.PORT || 3000
 
